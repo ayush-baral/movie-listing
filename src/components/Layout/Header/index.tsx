0234c1d20da51ef735662ba81f95d9ffe612f32e
Ayush <ayushbaral17@gmail.com>
2024-08-15 23:55:36 +0545

    feat: :lipstick: improve UI of header component

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -9,17 +9,17 @@ const Header = () => {
   };
 
   return (
-    <div className='bg-gray-200 w-full p-4 flex items-center justify-center placeholder:text-xs'>
-      <div className=''>
+    <header className='bg-gray-800 text-white p-4 flex items-center justify-center shadow-md'>
+      <div className='w-full max-w-lg'>
         <input
-          placeholder='Enter Movie Title'
+          placeholder='Search for movies...'
           type='text'
-          className='outline-none py-1 px-2 rounded-sm'
+          className='w-full py-2 px-4 rounded-lg border border-gray-600 bg-gray-900 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300'
           value={searchText}
           onChange={handleSearchInputChange}
         />
       </div>
-    </div>
+    </header>
   );
 };
 
